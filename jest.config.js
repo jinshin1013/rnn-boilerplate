b5bdfa20c5dce80b5036394e8345d02a3c09deec
Jin Shin <jinshin1013@gmail.com>
2019-01-23 16:11:05 +1000

    Fix ts-jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const { defaults: tsjPreset } = require('ts-jest/presets')
 
 module.exports = {
   ...tsjPreset,
-  presets: 'react-native',
+  preset: 'react-native',
   transform: {
     ...tsjPreset.transform,
     '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
