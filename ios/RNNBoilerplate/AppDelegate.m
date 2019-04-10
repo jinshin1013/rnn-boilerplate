0ccd57508f2e1955ac6eea955e30c1f0fa5d6c86
Jin Shin <jinshin1013@gmail.com>
2019-04-10 15:44:16 +1000

    Updated iOS files

--- ios/RNNBoilerplate/AppDelegate.m
+++ ios/RNNBoilerplate/AppDelegate.m
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
@@ -15,10 +8,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  NSURL *jsCodeLocation;
-
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
   [ReactNativeNavigation bootstrap:jsCodeLocation launchOptions:launchOptions];
+  
   return YES;
 }
 
